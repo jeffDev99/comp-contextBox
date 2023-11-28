961540332acb6d4c0880bcc3482b1d3a26f17938
S.Mohammad.Amin.Jafarnezhad <83462403+jeffDev99@users.noreply.github.com>
2023-11-28 14:29:30 +0330

    add basic files

---
+++ js/script.js
@@ -0,0 +1,26 @@
+﻿let $ = document;
+let bodyEl = $.body
+let contextMenuEl =$.querySelector("#contextMenu")
+
+bodyEl.addEventListener("contextmenu",(e)=>{
+e.preventDefault();
+console.log(e.pageX , e.pageY);
+contextMenuEl.style.left=e.pageX
+contextMenuEl.style.top=e.pageY
+contextMenuEl.style.display="block"
+})
+
+bodyEl.addEventListener("click",()=>{
+    contextMenuEl.style.left="0"
+    contextMenuEl.style.top="0"
+    contextMenuEl.style.display="none"
+})
+
+
+bodyEl.addEventListener("keydown",(e)=>{
+    if(e.key === "Escape"){
+        contextMenuEl.style.left="0"
+        contextMenuEl.style.top="0"
+        contextMenuEl.style.display="none"
+    }
+})
